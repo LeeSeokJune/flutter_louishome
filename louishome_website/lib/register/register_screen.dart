63a60a5ef5f06240c4aaa7909bfe74794da63ca1
LeeSeokJune <indians1720@gmail.com>
2022-04-25 17:12:50 +0900

    register screen

--- louishome_website/lib/register/register_screen.dart
+++ louishome_website/lib/register/register_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:louishome_website/controller/user_controller.dart';
+import 'package:louishome_website/data/constants.dart';
 import 'package:louishome_website/screens/components/bottomBar.dart';
 
 import '../screens/components/topAppBar.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({Key? key}) : super(key: key);
-
+  RegisterScreen({Key? key}) : super(key: key);
+  var userController = Get.put(UserController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,10 +31,139 @@ class RegisterScreen extends StatelessWidget {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 100),
+        RegisterInputField(),
+        SizedBox(height: 50),
         SSOButton(),
         SizedBox(height: 100),
         BottomBar(),
       ],
+    );
+  }
+
+  Widget RegisterInputField() {
+    return Column(
+      children: [
+        Container(
+          width: 400,
+          height: 50,
+          child: TextFormField(
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              disabledBorder: _buildOutLineInputBorder(),
+              enabledBorder: _buildOutLineInputBorder(),
+              focusedBorder: _buildOutLineInputBorder(),
+              hintText: '아이디 또는 이메일을 입력해주세요.',
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+              contentPadding: EdgeInsets.only(top: 1, left: 10),
+            ),
+            onSaved: (value) {
+              userController.user.value.id = value!;
+            },
+          ),
+        ),
+        Container(
+          width: 400,
+          height: 50,
+          child: TextFormField(
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              disabledBorder: _buildOutLineInputBorder(),
+              enabledBorder: _buildOutLineInputBorder(),
+              focusedBorder: _buildOutLineInputBorder(),
+              hintText: '이름을 입력해주세요.',
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+              contentPadding: EdgeInsets.only(top: 1, left: 10),
+            ),
+            onSaved: (value) {
+              userController.user.value.id = value!;
+            },
+          ),
+        ),
+        Container(
+          width: 400,
+          height: 50,
+          child: TextFormField(
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              disabledBorder: _buildOutLineInputBorder(),
+              enabledBorder: _buildOutLineInputBorder(),
+              focusedBorder: _buildOutLineInputBorder(),
+              hintText: '패스워드를 입력해주세요',
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+              contentPadding: EdgeInsets.only(top: 1, left: 10),
+            ),
+            onSaved: (value) {
+              userController.user.value.id = value!;
+            },
+          ),
+        ),
+        Container(
+          width: 400,
+          height: 50,
+          child: TextFormField(
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+              disabledBorder: _buildOutLineInputBorder(),
+              enabledBorder: _buildOutLineInputBorder(),
+              focusedBorder: _buildOutLineInputBorder(),
+              hintText: '패스워드 확인',
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+              contentPadding: EdgeInsets.only(top: 1, left: 10),
+            ),
+            onSaved: (value) {
+              userController.user.value.id = value!;
+            },
+          ),
+        ),
+        InkWell(
+          child: Container(
+            width: 400,
+            height: 50,
+            decoration: BoxDecoration(
+              color: louisColor,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Center(
+              child: Text(
+                '회원가입',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          onTap: () {
+            userController.formKey.value.currentState!.save();
+            print(userController.user.value);
+          },
+        ),
+      ],
+    );
+  }
+
+  OutlineInputBorder _buildOutLineInputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide(
+        color: louisColor,
+        width: 2,
+      ),
     );
   }
 
